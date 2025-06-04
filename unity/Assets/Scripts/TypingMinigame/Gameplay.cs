ef4e700cb8ebc1144adc1de6fabc2f8af9f30cd3
Skyerial <skyerial@skyerial.home>
2025-06-04 18:09:19 +0200

    prototype

---
+++ unity/Assets/Scripts/TypingMinigame/Gameplay.cs
@@ -0,0 +1,51 @@
+using TMPro;
+using UnityEditor.Overlays;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class Gameplay : MonoBehaviour
+{
+    private bool _gameStart = false;
+    private InputAction _enableAction;
+    public TMP_InputField _inputField;
+    private int _wordCount = 0;
+    public GameObject _spawner;
+    public HorizontalLayoutGroup _layoutGroup;
+
+    private void Start()
+    {
+        _enableAction = InputSystem.actions.FindAction("GameStart");
+        _inputField.onValueChanged.AddListener(HandleText);
+    }
+
+    void Update()
+    {
+        if (_enableAction.WasReleasedThisFrame() && !_gameStart)
+        {
+            _gameStart = true;
+        }
+
+        if (_wordCount == 5)
+        {
+            Debug.Log("won");
+        }
+    }
+
+    void HandleText(string text)
+    {
+        string userInput = _inputField.text;
+        Debug.Log("Player typed: " + userInput);
+        TextMeshPro currentText = GameObject.Find("Child_" + _wordCount).GetComponent<TextMeshPro>();
+
+
+        if (userInput == currentText.text)
+        {
+            Destroy(currentText);
+            var child = _spawner.transform.GetChild(0);
+            Destroy(child.gameObject);
+            _wordCount += 1;
+            _inputField.text = "";
+        }
+    }
+}
---
+++ unity/Assets/Scripts/TypingMinigame/TextSpawner.cs
@@ -0,0 +1,38 @@
+using JetBrains.Annotations;
+using TMPro;
+using UnityEngine;
+
+
+// words come at the player, player 'kills' the words
+// every word is its own object, gives easier control over movement and animations per word
+// counter # remaining on the right
+// words hop on moving
+// have two text boxes, one what the player types, one what the player needs to type, this way you can do checks and maybe color the right or wrong letters as well
+
+
+public class TextSpawner : MonoBehaviour
+{
+
+    public Transform spawner;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        TextMeshPro textPlayer1 = GameObject.Find("TextPlayer1").GetComponent<TextMeshPro>();
+        textPlayer1.text = "test word game mario track";
+
+        for (int word = 0; word < 5; word++)
+        {
+            TextMeshPro obj = Instantiate(textPlayer1, spawner);
+            obj.name = "Child_" + word;
+            obj.text = "test" + word;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
