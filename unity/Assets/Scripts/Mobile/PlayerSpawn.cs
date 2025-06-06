8deed38e7c6a70fa5e7371436ce84ffcce84fc61
JonasSkolnik <149402215+JonasSkolnik@users.noreply.github.com>
2025-06-06 12:36:18 +0200

    lobby functionality version 0.1

--- unity/Assets/Scripts/Mobile/PlayerSpawn.cs
+++ unity/Assets/Scripts/Mobile/PlayerSpawn.cs
@@ -12,6 +12,8 @@ public class PlayerSpawn : MonoBehaviour
         Transform Spawn = SpawnOBJ.GetComponent<Transform>();
         playerInput.transform.position = Spawn.transform.position;
 
+        
+
         // Adding a custom face
         // var face = playerInput.transform.Find("Face");
         // var renderer = face.GetComponent<Renderer>();
