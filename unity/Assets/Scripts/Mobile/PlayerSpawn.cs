212f410481191b93ee979fbfa03fc1533c58d1e1
JonasSkolnik <149402215+JonasSkolnik@users.noreply.github.com>
2025-06-08 16:19:06 +0200

    lobby 0.1.3

--- unity/Assets/Scripts/Mobile/PlayerSpawn.cs
+++ unity/Assets/Scripts/Mobile/PlayerSpawn.cs
@@ -6,45 +6,12 @@ using UnityEngine.UI;
 
 public class PlayerSpawn : MonoBehaviour
 {
-
-    public Transform[] playerSlots;
-    private int nextSlotIndex = 0;
     public void OnPlayerJoined(PlayerInput playerInput)
     {
-
-        if (nextSlotIndex >= playerSlots.Length)
-        {
-            Debug.LogWarning("All player slots are full!");
-            return;
-        }
-
-        Transform slot = playerSlots[nextSlotIndex];
-        playerInput.transform.SetParent(slot);
-        playerInput.transform.localPosition = Vector3.zero;
-        playerInput.transform.localRotation = Quaternion.identity;
-         playerInput.transform.localScale = new Vector3(100f, 100f, 1f);
-
-      
-        nextSlotIndex++;
-        // GameObject SpawnOBJ = GameObject.Find("Spawn");
-        // Transform Spawn = SpawnOBJ.GetComponent<Transform>();
-        // playerInput.transform.position = Spawn.transform.position;
-
-        // GameObject playerCard = GameObject.Find("Playercard");
-        // if (playerCard != null)
-        // {
-        //     playerInput.transform.SetParent(playerCard.transform);
-        // }
-        // else
-        // {
-        //     Debug.LogWarning("Playercard object not found in the scene.");
-        // }
-
-        // // Verander de schaal van de speler
-        // playerInput.transform.localScale = new Vector3(100f, 100f, 1000f);
-
-
-
+        GameObject SpawnOBJ = GameObject.Find("Spawn");
+        Transform Spawn = SpawnOBJ.GetComponent<Transform>();
+        playerInput.transform.position = Spawn.transform.position;
+        
 
         // Adding a custom face
         // var face = playerInput.transform.Find("Face");
