fadd11101181a21c285a3c22d474020d736cac94
JonasSkolnik <149402215+JonasSkolnik@users.noreply.github.com>
2025-06-10 17:44:11 +0200

    lobby 1.3
    Playercard added

--- unity/Assets/Scripts/Mobile/PlayerSpawn.cs
+++ unity/Assets/Scripts/Mobile/PlayerSpawn.cs
@@ -1,13 +1,22 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
 using System.Linq;
 using UnityEngine.TextCore;
 using UnityEngine.UI;
+using System;
 
 public class PlayerSpawn : MonoBehaviour
 {
-   public void OnPlayerJoined(PlayerInput playerInput)
+
+    void Start()
+    {
+        //Remove players from the lobby scene 
+        string currentScene = SceneManager.GetActiveScene().name;
+        if (currentScene == "Lobby") ServerManager.allControllers?.Clear();
+    }
+    public void OnPlayerJoined(PlayerInput playerInput)
     {
         GameObject playersParent = GameObject.Find("Players");
         // if not in lobby. 
@@ -19,18 +28,19 @@ public class PlayerSpawn : MonoBehaviour
         }
         else
         {
-            Transform spawnObj = GameObject.Find("Spawn").transform;
-
             Transform emptySlot = null;
             for (int i = 0; i < playersParent.transform.childCount; i++)
             {
                 Transform playerCard = playersParent.transform.GetChild(i);
-
+                Transform playerWaitingFrame = playerCard.Find("waitingFrame");
+                Transform characterFrame = playerCard.Find("PlayerCharacterFrame");
+                Transform characterShow = characterFrame.Find("characterShow");
                 bool hasPlayer = false;
-                foreach (Transform child in playerCard)
+                foreach (Transform child in characterShow)
                 {
                     if (child.GetComponent<PlayerInput>() != null)
                     {
+                        Debug.Log("Player found in characterShow");
                         hasPlayer = true;
                         break;
                     }
@@ -38,9 +48,13 @@ public class PlayerSpawn : MonoBehaviour
 
                 if (!hasPlayer)
                 {
-                    emptySlot = playerCard;
+                    emptySlot = characterShow;
+                    Debug.Log("Player not found in characterShow, looking for empty slot");
+                    playerWaitingFrame.gameObject.SetActive(false);
+                    characterFrame.gameObject.SetActive(true);
                     break;
                 }
+
             }
 
             if (emptySlot == null)
@@ -48,15 +62,11 @@ public class PlayerSpawn : MonoBehaviour
                 Debug.LogWarning("No empty player slot found!");
                 return;
             }
-
             playerInput.transform.SetParent(emptySlot, false);
-            float offsetY = -emptySlot.GetComponent<RectTransform>().rect.height * 0.30f;
+            float offsetY = -emptySlot.GetComponent<RectTransform>().rect.height * 0.15f;
             playerInput.transform.localPosition = new Vector3(0f, offsetY, 0f);
             playerInput.transform.localRotation = Quaternion.identity;
             playerInput.transform.localScale = Vector3.one * 150f;
-
-            spawnObj.SetParent(emptySlot, false);
-            spawnObj.localPosition = Vector3.zero;
         }
     }
 }
