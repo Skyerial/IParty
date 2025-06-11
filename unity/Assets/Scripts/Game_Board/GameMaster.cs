43cf6e729354aa591b2a1eecd1408f8dbe1ef053
jpokemon232 <mailerbotmailerbot@gmail.com>
2025-06-11 20:49:04 +0200

    gima borndozo

---
+++ unity/Assets/Scripts/Game_Board/GameMaster.cs
@@ -0,0 +1,48 @@
+using TMPro;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    private int current_player = -1;
+    public int change_player = 0;
+    public GameObject[] players;
+    public TextMeshProUGUI numberText;
+    public int press_random = 0;
+
+    private bool numberShown = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (current_player != change_player){
+            current_player = change_player;
+            for (int i = 0; i < players.Length; i++)
+            {
+                // Get the Camera component from the child object
+                Camera cam = players[i].GetComponentInChildren<Camera>(true); // true = include inactive
+                if (cam != null)
+                {
+                    cam.gameObject.SetActive(i == current_player);
+                }
+            }
+        }
+
+        if (press_random == 1 && !numberShown)
+        {
+            int randomNumber = Random.Range(1, 6); // change range as needed
+            //numberText.text = randomNumber.ToString();
+            //numberShown = true; // Prevent multiple updates
+            players[current_player].GetComponent<PlayerMovement>().increment = randomNumber;
+            numberText.text = randomNumber.ToString();
+            press_random = 2;
+        }
+        if (press_random == 2 && players[current_player].GetComponent<PlayerMovement>().increment == 0) {
+            change_player = (current_player + 1) % players.Length;
+            press_random = 0;
+        }
+    }
+}
---
+++ unity/Assets/Scripts/Game_Board/script.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class script : MonoBehaviour
+{
+    [Header("Spiral Settings")]
+    public float radius = 5f;
+    public float height = 5f;
+    public Vector3 TargetPos;
+    public GameObject objectToClone;
+    public int step_distance = 1;
+
+    [Header("Runtime")]
+    public bool regenerate = false;
+    void Start()
+    {
+        if (transform.parent != null)
+        {
+            string parentName = transform.parent.name;
+            Debug.Log("Parent name: " + parentName);
+        }
+        else
+        {
+            Debug.Log("This GameObject has no parent.");
+        }
+    }
+    private void Update()
+    {
+        if (regenerate)
+        {
+            regenerate = false;
+
+
+            ClearChildren();
+            GenerateSpiral();
+
+        }
+    }
+
+    void GenerateSpiral()
+    {
+        float b = -radius / (8 * 3.14f);
+        float a = radius;
+        float theta = 0;
+
+        for (int i = 0; i <= 1000; i++) {
+            float r = a + b * theta;
+            if (r < 0) {
+                return;
+            }
+            float x = TargetPos.x + r * Mathf.Cos(theta);
+            float dynamicY = TargetPos.y + (1 - (r / radius)) * height;
+            float z = TargetPos.z + r * Mathf.Sin(theta);
+
+            Vector3 pos = new Vector3(
+                x,
+                dynamicY,
+                z
+            );
+            float dr_dtheta = b;
+            float ds_dtheta = Mathf.Sqrt(dr_dtheta*dr_dtheta + r*r);
+
+            float dtheta = step_distance / ds_dtheta;
+            theta = theta + dtheta;
+
+            GameObject clone = Instantiate(objectToClone, pos, Quaternion.identity, transform);
+
+
+        }
+        
+    }
+
+    void ClearChildren()
+    {
+        for (int i = transform.childCount - 1; i >= 0; i--)
+        {
+            Transform child = transform.GetChild(i);
+            if (child.name == "Stump_01(Clone)")
+            {
+                DestroyImmediate(child.gameObject);
+            }
+        }
+    }
+}
