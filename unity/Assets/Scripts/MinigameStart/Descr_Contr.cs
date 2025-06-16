49873e1532149755deae99bf1fac7bf225100ca2
SirosukeZero <ryomanonaka@gmail.com>
2025-06-16 11:57:23 +0200

    button removed

--- unity/Assets/Scripts/MinigameStart/Descr_Contr.cs
+++ unity/Assets/Scripts/MinigameStart/Descr_Contr.cs
@@ -3,7 +3,7 @@ public class Descr_Contr : MonoBehaviour
 {
     int clickCount = 0;
 
-    public void goofyName()
+    public void OnClick()
     {
         clickCount++;
         Debug.Log("Clicked " + clickCount + " times");
