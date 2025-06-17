8fedf963ecb76ead218ae6c8064ee5414bb56ade
saleeman <saleemaany@gmail.com>
2025-06-17 13:47:50 +0200

    switch joystick/buttons to pointer events for true multitouch and added better styling for button

--- unity/Assets/StreamingAssets/controllers/components/buttonsComponent.js
+++ unity/Assets/StreamingAssets/controllers/components/buttonsComponent.js
@@ -1,22 +1,42 @@
 import { ViewRenderer } from '../../utils/viewRenderer.js';
 import { socketManager } from '../../main.js';
 
-export class ButtonsComponent extends ViewRenderer{
+export class ButtonsComponent extends ViewRenderer {
   constructor(container, vertical = false) {
-    super("./views/components/buttonsComponentView.html", container, "buttonInput");
+    super(
+      "./views/components/buttonsComponentView.html",
+      container,
+      "buttonInput"
+    );
     this.vertical = vertical;
   }
 
   bindEvents() {
     if (this.vertical) {
-      this.container.querySelector('.button-container')
-                    .classList.add('orientation-vertical');
+      this.container
+        .querySelector('.button-container')
+        .classList.add('orientation-vertical');
     }
 
     this.container.querySelectorAll('.game-button').forEach(button => {
       const name = button.innerText;
-      button.addEventListener('touchstart', () => socketManager.updateButton(name, true));
-      button.addEventListener('touchend',   () => socketManager.updateButton(name, false));
+
+      button.addEventListener('pointerdown', e => {
+        if (e.pointerType !== 'touch') return;
+        socketManager.updateButton(name, true);
+        button.setPointerCapture(e.pointerId);
+      });
+
+      button.addEventListener('pointerup', e => {
+        if (e.pointerType !== 'touch') return;
+        socketManager.updateButton(name, false);
+        button.releasePointerCapture(e.pointerId);
+      });
+
+      button.addEventListener('pointercancel', e => {
+        if (e.pointerType !== 'touch') return;
+        socketManager.updateButton(name, false);
+      });
     });
   }
 }
