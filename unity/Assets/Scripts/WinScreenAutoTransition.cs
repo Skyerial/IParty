4cd72d5a6ef9f775d7cb70a394ca96a9fbb6966b
SirosukeZero <ryomanonaka@gmail.com>
2025-06-18 14:00:10 +0200

    added autotransision and added small hud to the gameboard

---
+++ unity/Assets/Scripts/WinScreenAutoTransition.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreenAutoTransition : MonoBehaviour
+{
+    [SerializeField] private float delayBeforeTransition = 2.5f; // You can adjust this in Inspector
+
+    private void Start()
+    {
+        Invoke(nameof(GoToGameBoard), delayBeforeTransition);
+    }
+
+    private void GoToGameBoard()
+    {
+        SceneManager.LoadScene("Game_Board");
+    }
+}
