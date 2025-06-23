13810b2522a444d3a8b9fa26730b097f30a5bea0
JonasSkolnik <149402215+JonasSkolnik@users.noreply.github.com>
2025-06-23 17:55:03 +0200

    Fixed settings in main menu
    Volume can be changed and fullscreen can be turned on

--- unity/Assets/Scripts/SettingScript.cs
+++ unity/Assets/Scripts/SettingScript.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingScript : MonoBehaviour
 {
     public GameObject settingsPanel;
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private Slider musicVolumeSlider;
     void Start()
     {
         settingsPanel.SetActive(false);
+        if (musicVolumeSlider != null)
+        {
+            musicVolumeSlider.onValueChanged.AddListener(OnMusicVolumeChanged);
+            musicVolumeSlider.value = audioSource.volume;
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMusicVolumeChanged(float value)
     {
-        
+        audioSource.volume = value;
+    }
+
+    public void Fullscreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
     }
 }
