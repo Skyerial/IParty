7272c9b81664d9d566eb2378a46e7dce79d56c1a
SirosukeZero <ryomanonaka@gmail.com>
2025-06-25 13:04:32 +0200

    added debug mode

---
+++ unity/Assets/Scripts/lobby/DebugSandboxButton.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DebugSandboxButton : MonoBehaviour
+{
+    [Header("Debug Toggle")]
+    [SerializeField] private bool enableDebugButton = false;
+
+    [Header("UI Settings")]
+    [SerializeField] private Vector2 buttonPosition = new Vector2(-100, -50);
+    [SerializeField] private Vector2 buttonSize = new Vector2(180, 40);
+    [SerializeField] private string sandboxSceneName = "MobileSandbox";
+    [SerializeField] private Font buttonFont; // <- Add this
+    public static bool DebugEnabled { get; private set; } = false;
+
+
+
+    private void Start()
+    {
+#if UNITY_EDITOR
+        if (!enableDebugButton) return;
+
+        string currentScene = SceneManager.GetActiveScene().name;
+        if (currentScene != "Lobby") return;
+
+        CreateDebugButton();
+#endif
+    }
+
+    private void CreateDebugButton()
+    {
+        Canvas canvas = Object.FindFirstObjectByType<Canvas>();
+        if (canvas == null)
+        {
+            Debug.LogWarning("No Canvas found for DebugSandboxButton.");
+            return;
+        }
+
+        GameObject buttonGO = new GameObject("DebugSandboxButton", typeof(RectTransform), typeof(CanvasRenderer), typeof(Image), typeof(Button));
+        buttonGO.transform.SetParent(canvas.transform, false);
+
+        RectTransform rect = buttonGO.GetComponent<RectTransform>();
+        rect.anchorMax = new Vector2(1, 1);
+        rect.anchorMin = new Vector2(1, 1);
+        rect.pivot = new Vector2(1, 1);
+        rect.anchoredPosition = buttonPosition;
+        rect.sizeDelta = buttonSize;
+
+        Image img = buttonGO.GetComponent<Image>();
+        img.color = Color.gray;
+
+        GameObject textGO = new GameObject("Text", typeof(RectTransform), typeof(Text));
+        textGO.transform.SetParent(buttonGO.transform, false);
+        RectTransform textRect = textGO.GetComponent<RectTransform>();
+        textRect.anchorMin = Vector2.zero;
+        textRect.anchorMax = Vector2.one;
+        textRect.offsetMin = Vector2.zero;
+        textRect.offsetMax = Vector2.zero;
+
+        Text text = textGO.GetComponent<Text>();
+        text.text = "Go Sandbox";
+        text.alignment = TextAnchor.MiddleCenter;
+        text.font = buttonFont != null ? buttonFont : Resources.GetBuiltinResource<Font>("LegacyRuntime.ttf");
+        text.color = Color.black;
+
+
+        Button btn = buttonGO.GetComponent<Button>();
+        btn.onClick.AddListener(() =>
+        {
+            Debug.Log("DEBUG button clicked: loading sandbox scene...");
+            SceneManager.LoadScene(sandboxSceneName);
+        });
+
+        DebugEnabled = true;
+
+
+    }
+}
