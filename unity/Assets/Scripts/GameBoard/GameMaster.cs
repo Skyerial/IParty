b73032300a8e3a78893db771af6af229f5eb4ec6
Owen Duddles <owen.duddles@student.uva.nl>
2025-06-25 19:32:49 +0100

    1 line fix

--- unity/Assets/Scripts/GameBoard/GameMaster.cs
+++ unity/Assets/Scripts/GameBoard/GameMaster.cs
@@ -39,6 +39,7 @@ public class GameMaster : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        clearTurnText();
         clearDiceText();
         diceCam = GameObject.Find("DiceCamera").GetComponent<Camera>();
         GameObject.Find("DiceCamera").SetActive(false);
