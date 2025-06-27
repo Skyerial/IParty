7f567323a5486a25bffb40d9cf4c66b7c2afaea5
Owen Duddles <owen.duddles@student.uva.nl>
2025-06-27 14:47:38 +0100

    color changer comments

--- unity/Assets/Scripts/Global/ColorChanger.cs
+++ unity/Assets/Scripts/Global/ColorChanger.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
 
+/**
+ * @brief Class used to change color of character sprite
+ */
 public class ColorChanger : MonoBehaviour
 {
     public Color newColor = Color.red;
 
+    /**
+    * @brief Start function called uppon script initiation
+    * handling the new color assigning of the character
+    */
     void Start()
     {
         Renderer renderer = GetComponent<Renderer>();
